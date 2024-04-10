2bdfb1e5748ebd42e9a35423a625bd72ca51bd3f
pixelplus <ict@pixelplus.nl>
2024-04-10 09:43:09 +0200

    Fix/ z-index + button hover search & book tile > hp

--- components/Homepage/SearchBookTile.tsx
+++ components/Homepage/SearchBookTile.tsx
@@ -3,7 +3,7 @@ import SearchBookDropdown from "./SearchBookDropdown";
 
 export default function SearchBookTile() {
   return (
-    <div className="flex justify-center">
+    <div className="flex justify-center relative z-20">
       <div className="bg-[#fff] items-center w-full mt-3  px-5 lg:px-10 lg:mx-5 xl:mx-0 rounded-3xl lg:rounded-full gap-8  py-5 lg:flex  justify-between text-[#556A76]">
         <h2 className="text-xl md:text-3xl lg:text-2xl text-center lg:text-left">
           Zoek & boek je
@@ -23,13 +23,11 @@ export default function SearchBookTile() {
             <SearchBookDropdown label="Slaapkamers" />
           </div>
         </div>
-        <div>
-          <Link
-            className="bg-[#A0AE9C] whitespace-nowrap flex items-center gap-2 w-fit m-auto mt-5 lg:mt-0 rounded-full  p-5   text-[#fff]"
-            href="">
-            Zoek & boek <span className="text-xl">→</span>
-          </Link>
-        </div>
+        <Link
+          className="bg-[#A0AE9C] whitespace-nowrap flex items-center gap-2 w-fit m-auto mt-5 lg:mt-0 rounded-full  p-5   text-[#fff]"
+          href="#">
+          Zoek & boek <span className="text-xl">→</span>
+        </Link>
       </div>
     </div>
   );
