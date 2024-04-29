8569577c763ee52536932a9b0970fa7c1dafb4ec
cedricmorreau <cedricmorreau@gmail.com>
2024-04-29 09:20:42 +0200

    Fix/ order detail page nav + center images "wat is er te doen"

--- components/Standard/PageNav.tsx
+++ components/Standard/PageNav.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function PageNav(props: any) {
   return (
-    <div className="flex justify-center items-center px-5 md:px-10 2xl:px-0 gap-x-5 gap-y-2 mt-3 flex-wrap max-w-3xl mx-auto">
+    <div className="flex flex-row-reverse justify-center items-center px-5 md:px-10 2xl:px-0 gap-x-5 gap-y-2 mt-3 flex-wrap-reverse max-w-3xl mx-auto">
       {props.nav.menu.menuItems.nodes.map((item: any, index: any) => {
         return (
           <Link href={item.uri} key={index}>
